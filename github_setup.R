061b2a559b3758fb2747b7cfe5c1ecd2fd021f23
millzyr <rrmills93@gmail.com>
2021-04-02 16:58:27 +1300

    First commit

---
+++ github_setup.R
@@ -0,0 +1,13 @@
+
+install.packages("usethis")
+
+install.packages("gitcreds")
+
+usethis::browse_github_token()
+
+gitcreds::gitcreds_set()
+
+# Type this in terminal tab
+# git config --global user.name "*ENTER USERNAME*"
+# git config --global user.email "*ENTER EMAIL*"
+
